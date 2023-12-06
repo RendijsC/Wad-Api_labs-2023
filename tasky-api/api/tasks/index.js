e3e7a99ac99262bd67bc923cf429c43c4fb5b3d6
RendijsC <20098325@mail.wit.ie>
2023-12-06 15:48:11 +0000

    Week 9 Lab Excercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -22,17 +22,22 @@ router.get('/:id', (req, res) => {
 //Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
+    const currentTime = new Date().toISOString();
+
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at: currentTime,
+        updated_at: currentTime
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
     tasksData.total_results++;
+    
 });
 
 //Update an existing task
@@ -42,7 +47,8 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const currentTime = new Date().toISOString();
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id: id, updated_at: currentTime };
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
