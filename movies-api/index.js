e6a92ad07cee45632000c89f33d33b8580be247c
RendijsC <20098325@mail.wit.ie>
2023-12-08 16:45:24 +0000

    Added Movies Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
+import moviesRouter from './api/movies';   //import movies router
+
 
 dotenv.config();
 
@@ -12,8 +14,10 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
